4f1b1a3b7334f37a78d4cbde254d95c1b5f0656d
thkim0515 <thkim0515@naver.com>
2024-05-29 15:35:01 +0900

    UPDATE:구조 변경

---
+++ src/pages/Page/MainPage.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 30px 20px;
+    gap: 20px;
+  }
+`;
