e6a927e4a95b2d6c75e848ceb7ee38951556a488
thkim0515 <thkim0515@naver.com>
2024-06-03 22:28:51 +0900

    UPDATE:SIGNUP

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useSelector } from "react-redux";
 import * as S from "./FirstPage.style";
+import SignUp from "../SignUp/SignUp";
 
 const FirstPage = () => {
   const count = useSelector((state) => state.counter.count);
@@ -13,6 +14,8 @@ const FirstPage = () => {
         <h1>{count}</h1>
         <div>페이지1</div>
         <h1>{user}</h1>
+        <br />
+        <SignUp />
       </S.Container>
     </>
   );
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SignUp = () => {
+  const [userId, setUserId] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userNickname, setUserNickname] = useState("");
+
+  const handleSignUp = async () => {
+    const userData = {
+      userId,
+      userPassword,
+      userNickname,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/users/register",
+        userData
+      );
+      console.log("User registered:", response.data);
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+        placeholder="User ID"
+      />
+      <input
+        type="password"
+        value={userPassword}
+        onChange={(e) => setUserPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <input
+        type="text"
+        value={userNickname}
+        onChange={(e) => setUserNickname(e.target.value)}
+        placeholder="Nickname"
+      />
+      <button onClick={handleSignUp}>Sign Up</button>
+    </div>
+  );
+};
+
+export default SignUp;
