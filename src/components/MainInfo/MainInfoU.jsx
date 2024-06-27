3e081efb52e7d0643767a04d7fb07f8533331ad5
jiandays <yj000747@naver.com>
2024-06-27 19:41:47 +0900

    sja_maidMainPageInfo

---
+++ src/components/MainInfo/MainInfoU.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import * as S from "./MainInfoU.style"; // 스타일링 파일을 별도로 import
+
+const MainInfoU = () => (
+	<S.LabelContainer>
+		<S.Text>
+			다양한 게시글을 통해서 서로의 생각을 나누는 소통의 공간
+		</S.Text>
+		<S.Box>
+			<S.Group>
+				<S.Overlap>
+					<S.Payment>
+						<S.OGroup>
+							<S.List>
+								{Array.from({ length: 4 }, (_, i) => (
+									i !== 0 ? (
+										<S.Element2 key={i}>
+											<S.Icon>
+												<S.Trending alt="Trending" src="trending.svg" />
+											</S.Icon>
+											<S.Text>
+												<S.TWrapper>게시글 제목</S.TWrapper>
+											</S.Text>
+											<S.Date>2 March 2021, 13:45 PM</S.Date>
+											<S.Likes>♥ : 0개</S.Likes>
+											<S.OGroup2>
+												<S.Author>작성자명</S.Author>
+											</S.OGroup2>
+										</S.Element2>
+									) : (
+										<S.Element key={i}>
+											<S.Icon>
+												<S.Trending alt="Trending" src="trending.svg" />
+											</S.Icon>
+											<S.Text>
+												<S.TWrapper>게시글 제목</S.TWrapper>
+											</S.Text>
+											<S.Date>2 March 2021, 13:45 PM</S.Date>
+											<S.Likes>♥ : 0개</S.Likes>
+											<S.OGroup2>
+												<S.Author>작성자명</S.Author>
+											</S.OGroup2>
+										</S.Element>
+									)
+								))}
+							</S.List>
+							<S.PostTitle>게시글</S.PostTitle>
+						</S.OGroup>
+					</S.Payment>
+					<S.Pagination>
+						<S.ShowingFrom
+							alt="Showing from"
+							src="showing-1-5-from-100-data.svg"
+						/>
+						<S.Page>
+							<S.Dropdown alt="Dropdown" src="image.svg" />
+							{Array.from({ length: 3 }, (_, i) => (
+								<S.OWrapper key={i}>
+									<S.Overlap2>
+										{i === 0 ? <S.TextWrapper6>{i + 1}</S.TextWrapper6> : <S.TextWrapper7>{i + 1}</S.TextWrapper7>}
+									</S.Overlap2>
+								</S.OWrapper>
+							))}
+							<S.Dropdown alt="Dropdown" src="dropdown.svg" />
+						</S.Page>
+					</S.Pagination>
+				</S.Overlap>
+			</S.Group>
+		</S.Box>
+	</S.LabelContainer>
+);
+
+export default MainInfoU;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { increase, decrease } from "../../redux/actions/counter";
-
+import axios from "axios";
+import PostList from "../../components/PostList";
 import * as S from "./MainPage.style";
+import MainInfoL from "../../components/MainInfo/MainInfoL";
+import MainInfoR from "../../components/MainInfo/MainInfoR";
+import MainInfoU from "../../components/MainInfo/MainInfoU";
+import MainInfoM from "../../components/MainInfo/MainInfoM";
+// `MainPage` 컴포넌트 정의
 const MainPage = () => {
-  const count = useSelector((state) => state.counter.count);
-  const value = useSelector((state) => state.init.value);
-  const user = useSelector((state) => state.user.user);
-  const generationValue = useSelector((state) => state.generation.value);
-  const dispatch = useDispatch();
+	const [data, setData] = useState(null);
 
-  return (
-    <S.Container>
-      <p>
-        {value} - {generationValue} - {user}
-      </p>
-      <div>
-        <h1>{count}</h1>
-        <Button variant="outline-primary" onClick={() => dispatch(increase())}>
-          증가
-        </Button>{" "}
-        <Button variant="outline-primary" onClick={() => dispatch(decrease())}>
-          감소
-        </Button>{" "}
-      </div>
-    </S.Container>
-  );
+	const onClick = async () => {
+		try {
+			const response = await axios.get(
+				"https://jsonplaceholder.typicode.com/todos/1"
+			);
+			setData(response.data);
+			console.log(response.data);
+		} catch (e) {
+			console.error(e);
+			alert("Error: 데이터를 불러올 수 없습니다.");
+		}
+	};
+
+	return (
+		<S.MainPage>
+			<S.Content>
+				<Button variant="outline-primary" onClick={onClick}>
+					데이터 불러오기
+				</Button>
+				{data && (
+					<div>
+						<h2>API 응답 데이터:</h2>
+						<p>ID: {data.id}</p>
+						<p>Title: {data.title}</p>
+						<p>Completed: {data.completed ? "True" : "False"}</p>
+					</div>
+				)}
+				<Element />
+				<Element withDummyData />
+				<Label />
+				<PostList />
+			</S.Content>
+		</S.MainPage>
+	);
 };
 
+// 최종 `MainPage` 컴포넌트를 export
 export default MainPage;
