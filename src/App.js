24c21edec9ae2bd5aec4f5a9e05d739d67e34e34
jiandays <yj000747@naver.com>
2024-07-01 10:20:53 +0900

    sja_revice3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import GlobalStyle from "./style/globalStyle";
 import Header from "./components/Header/Header";
-import SideBar from "./components/SideBar/SideBar";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import MainPage from "./pages/MainPage/MainPage";
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
 import ChatButton from "./components/Common/ChatButton/ChatButton";
-import { Routes, Route } from "react-router-dom";
+import SideBar from "./components/SideBar/SideBar";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+// 임시 추가된 컴포넌트들 (실제로는 독립된 파일로 구성)
+const StudentList = () => <div>Student List Page</div>;
+const Course = () => <div>Course Page</div>;
+const Batch = () => <div>Batch Page</div>;
+const Chat = () => <div>Chat Page</div>;
+const Settings = () => <div>Settings Page</div>;
+const Logout = () => <div>Logout Page</div>;
 
 function App() {
 	return (
@@ -16,16 +22,21 @@ function App() {
 			<GlobalStyle />
 			<div className="app">
 				<Header />
-				<main className="content">
+				<div className="main-layout">
 					<SideBar />
-					<Routes>
-						<Route path="/" element={<Main />} />
-						<Route path="/MainPage" element={<MainPage />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<SignUp />} />
-					</Routes>
-					<ChatButton />
-				</main>
+					<main className="content">
+						<Routes>
+							<Route path="/" element={<Main />} />
+							<Route path="/student-list" element={<StudentList />} />
+							<Route path="/course" element={<Course />} />
+							<Route path="/batch" element={<Batch />} />
+							<Route path="/chat" element={<Chat />} />
+							<Route path="/settings" element={<Settings />} />
+							<Route path="/logout" element={<Logout />} />
+						</Routes>
+						<ChatButton />
+					</main>
+				</div>
 				<Footer />
 			</div>
 		</>
