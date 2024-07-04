62dde80137d15ff814869bfcf406e61395cd80c4
jiandays <yj000747@naver.com>
2024-07-04 16:36:46 +0900

    sja_revice2

---
+++ src/pages/SignUp/SignUp.style.js
@@ -0,0 +1,469 @@
+import styled from "styled-components";
+
+export const SignUp = styled.div`
+	background-color: #ffffff;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	width: 100%;
+`;
+
+export const Div = styled.div`
+	background-color: #ffffff;
+	height: 2742px;
+	position: relative;
+	width: 1509px;
+`;
+
+export const Button = styled.button`
+	all: unset;
+	background-color: #005b99;
+	border-radius: 26px;
+	box-sizing: border-box;
+	height: 62px;
+	left: 1095px;
+	overflow: hidden;
+	position: absolute;
+	top: 1892px;
+	width: 232px;
+`;
+
+export const Submit = styled.div`
+	color: #ffffff;
+	font-family: "Noto Sans-Regular", Helvetica;
+	font-size: 20px;
+	font-weight: 400;
+	left: 76px;
+	letter-spacing: 0;
+	line-height: 28px;
+	position: absolute;
+	text-align: center;
+	top: 16px;
+	white-space: nowrap;
+`;
+
+export const BookAnAppointment = styled.div`
+	color: #1a4e8d;
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 40px;
+	font-weight: 400;
+	left: 131px;
+	letter-spacing: 0;
+	line-height: 48px;
+	position: absolute;
+	text-align: center;
+	top: 478px;
+	width: 1127px;
+`;
+
+export const DivWrapper = styled.button`
+	all: unset;
+	background-color: #005b99;
+	border-radius: 26px;
+	box-sizing: border-box;
+	height: 59px;
+	left: 1095px;
+	overflow: hidden;
+	position: absolute;
+	top: 1632px;
+	width: 125px;
+`;
+
+export const TextWrapper = styled.div`
+	color: #ffffff;
+	font-family: "Noto Sans-Regular", Helvetica;
+	font-size: 20px;
+	font-weight: 400;
+	left: 20px;
+	letter-spacing: 0;
+	line-height: 28px;
+	position: absolute;
+	text-align: center;
+	top: 10px;
+	white-space: nowrap;
+	width: 87px;
+`;
+
+export const FirstName = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1455px;
+	width: 636px;
+`;
+
+export const TextWrapper2 = styled.div`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 22px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: normal;
+	margin-top: -1px;
+	position: relative;
+	width: 192px;
+`;
+
+export const Form = styled.div`
+	height: 48px;
+	position: relative;
+	width: 619px;
+`;
+
+export const OverlapGroup = styled.div`
+	background-color: var(--colorwhite);
+	border: 1px solid;
+	border-color: #c1bbeb;
+	border-radius: 5px;
+	height: 48px;
+	position: relative;
+	width: 617px;
+`;
+
+export const TextWrapper3 = styled.div`
+	color: var(--colorgray-3);
+	font-family: var(--text-text-regular-2-font-family);
+	font-size: var(--text-text-regular-2-font-size);
+	font-style: var(--text-text-regular-2-font-style);
+	font-weight: var(--text-text-regular-2-font-weight);
+	left: 13px;
+	letter-spacing: var(--text-text-regular-2-letter-spacing);
+	line-height: var(--text-text-regular-2-line-height);
+	position: absolute;
+	top: 13px;
+`;
+
+export const Phone = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1586px;
+	width: 636px;
+`;
+
+export const TextWrapper4 = styled.div`
+	color: var(--colorgray-3);
+	font-family: var(--text-text-regular-2-font-family);
+	font-size: var(--text-text-regular-2-font-size);
+	font-style: var(--text-text-regular-2-font-style);
+	font-weight: var(--text-text-regular-2-font-weight);
+	left: 13px;
+	letter-spacing: var(--text-text-regular-2-letter-spacing);
+	line-height: var(--text-text-regular-2-line-height);
+	position: absolute;
+	top: 14px;
+	width: 551px;
+`;
+
+export const Email = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 106px;
+	left: 380px;
+	position: absolute;
+	top: 1717px;
+	width: 636px;
+`;
+
+export const SubmitWrapper = styled.button`
+	all: unset;
+	background-color: #005b99;
+	border-radius: 26px;
+	box-sizing: border-box;
+	height: 60px;
+	left: 1095px;
+	overflow: hidden;
+	position: absolute;
+	top: 1116px;
+	width: 125px;
+`;
+
+export const Submit2 = styled.div`
+	color: #ffffff;
+	font-family: "Noto Sans-Regular", Helvetica;
+	font-size: 20px;
+	font-weight: 400;
+	left: 23px;
+	letter-spacing: 0;
+	line-height: 28px;
+	position: absolute;
+	text-align: center;
+	top: 15px;
+	white-space: nowrap;
+`;
+
+export const LastName = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1072px;
+	width: 636px;
+`;
+
+export const TextWrapper5 = styled.div`
+	color: var(--colorgray-3);
+	font-family: var(--text-text-regular-2-font-family);
+	font-size: var(--text-text-regular-2-font-size);
+	font-style: var(--text-text-regular-2-font-style);
+	font-weight: var(--text-text-regular-2-font-weight);
+	left: 13px;
+	letter-spacing: var(--text-text-regular-2-letter-spacing);
+	line-height: var(--text-text-regular-2-line-height);
+	position: absolute;
+	top: 14px;
+`;
+
+export const Photo = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 236px;
+	left: 604px;
+	position: absolute;
+	top: 621px;
+	width: 206px;
+`;
+
+export const OverlapGroupWrapper = styled.div`
+	height: 175px;
+	position: relative;
+	width: 177px;
+`;
+
+export const DragAndDropOrWrapper = styled.div`
+	background-color: var(--colorwhite);
+	border: 1px dashed;
+	border-color: #c1bbeb;
+	border-radius: 5px;
+	height: 175px;
+	position: relative;
+	width: 175px;
+`;
+
+export const DragAndDropOr = styled.p`
+	position: relative;
+	top: 50%;
+	transform: translateY(-50%);
+	text-align: center;
+`;
+
+export const TextWrapper6 = styled.div`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 22px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: normal;
+	margin-top: -1px;
+	position: relative;
+	width: 192px;
+`;
+
+export const View = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1586px;
+	width: 636px;
+`;
+
+export const LastName2 = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1727px;
+	width: 636px;
+`;
+
+export const NickName = styled.div`
+	color: var(--colorgray-3);
+	font-family: var(--text-text-regular-2-font-family);
+	font-size: var(--text-text-regular-2-font-size);
+	font-style: var(--text-text-regular-2-font-style);
+	font-weight: var(--text-text-regular-2-font-weight);
+	left: 13px;
+	letter-spacing: var(--text-text-regular-2-letter-spacing);
+	line-height: var(--text-text-regular-2-line-height);
+	position: absolute;
+	top: 14px;
+`;
+
+export const View2 = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	height: 105px;
+	left: 380px;
+	position: absolute;
+	top: 1858px;
+	width: 636px;
+`;
+
+export const Header = styled.img`
+	width: 100%;
+	height: auto;
+`;
+
+export const Footer = styled.footer`
+	background-color: #f1f1f1;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	padding: 20px;
+	position: relative;
+	width: 100%;
+`;
+
+export const P = styled.p`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 14px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: 21px;
+	text-align: center;
+	width: 100%;
+`;
+
+export const TextWrapper7 = styled.p`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 14px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: 21px;
+	text-align: center;
+	width: 100%;
+`;
+
+export const TextWrapper8 = styled.div`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 14px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: 21px;
+	text-align: center;
+	width: 100%;
+`;
+
+export const Container = styled.div`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	position: relative;
+	width: 100%;
+`;
+
+export const Pawsitively = styled.div`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 24px;
+	font-weight: 700;
+	letter-spacing: 0;
+	line-height: 36px;
+	text-align: center;
+`;
+
+export const HeaderMenu = styled.div`
+	display: flex;
+	flex-direction: row;
+	gap: 20px;
+	justify-content: center;
+	width: 100%;
+`;
+
+export const Frame = styled.div`
+	color: var(--colortextmenu);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 16px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: 24px;
+	text-align: center;
+	cursor: pointer;
+`;
+
+export const TextWrapper9 = styled.div`
+	color: var(--colortext);
+	font-family: "Poppins-Regular", Helvetica;
+	font-size: 16px;
+	font-weight: 400;
+	letter-spacing: 0;
+	line-height: 24px;
+	text-align: center;
+`;
+
+export const Frame2 = styled.div`
+	border-radius: 6px;
+	height: 44px;
+	left: 98px;
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	width: 100px;
+`;
+
+export const LocationHoursWrapper = styled.div`
+	border-radius: 6px;
+	height: 44px;
+	left: 198px;
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	width: 160px;
+`;
+
+export const Frame3 = styled.div`
+	border-radius: 6px;
+	height: 44px;
+	left: 358px;
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	width: 67px;
+`;
+
+export const Frame4 = styled.div`
+	border-radius: 6px;
+	height: 44px;
+	left: 425px;
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	width: 90px;
+`;
+
+export const Group = styled.div`
+	height: 24px;
+	left: 1290px;
+	position: absolute;
+	top: 165px;
+	width: 144px;
+`;
