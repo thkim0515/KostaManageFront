d52a4bfaf80d3195ec47ac12a5624abe6ac0df53
jiandays <yj000747@naver.com>
2024-07-12 02:18:19 +0900

    sja_maid_branchManage

---
+++ src/components/BranchManage/BranchManage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BranchManage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+//학원 지점 이름: branchName
+//학원 지점 위치/주소: branchLocation
+//학원 지점 전화번호: branchPhoneNumber
+//학원 지점 이메일 주소: branchEmail
+//학원 지점 운영시간: branchHours
+//학원 지점 담당자 정보 (이름, 전화번호, 이메일):
+//이름: managerName
+//전화번호: managerPhoneNumber
+//이메일: managerEmail
+export default BranchManage;
---
+++ src/components/BranchManage/BranchManage.style.js
@@ -0,0 +1,9 @@
+//BranchName
+//BranchLocation
+//BranchPhoneNumBer
+//BranchEmail
+//BranchHours
+//학원 지점 담당자 정보 (이름, 전화번호, 이메일):
+//ManagerName
+//ManagerPhoneNumber
+//ManagerEmail
