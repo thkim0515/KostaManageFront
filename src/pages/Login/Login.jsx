9bae4da9cca6e7feeacd560b2fac33e27d826a69
jiandays <yj000747@naver.com>
2024-07-11 15:46:05 +0900

    sja_reviceLogin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,52 +2,59 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/actions/userLoginSession";
 import { handleLogin } from "../../utils/login";
-import { Button } from "react-bootstrap";
+import * as S from "./Login.style";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const user = useSelector((state) => state.user.user);
-  const dispatch = useDispatch();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const user = useSelector((state) => state.user.user);
+	const dispatch = useDispatch();
 
-  const handleLoginClick = () => {
-    handleLogin(username, password, dispatch);
-  };
+	const handleLoginClick = () => {
+		handleLogin(username, password, dispatch);
+	};
 
-  const handleLogout = () => {
-    dispatch(logout());
-  };
+	const handleLogout = () => {
+		dispatch(logout());
+	};
 
-  return (
-    <div>
-      {!user ? (
-        <>
-          <input
-            type="text"
-            placeholder="아이디"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="패스워드"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button variant="outline-primary" onClick={handleLoginClick}>
-            Login
-          </Button>
-        </>
-      ) : (
-        <>
-          <p>로그인 아이디 : {user}</p>
-          <Button variant="outline-primary" onClick={handleLogout}>
-            Logout
-          </Button>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<S.Container>
+			{!user ? (
+				<S.Group>
+					<h2>로그인</h2>
+					<br />
+					<br />
+					<S.LoginField>
+						<S.Input
+							type="text"
+							placeholder="아이디"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</S.LoginField>
+					<S.LoginField>
+						<S.Input
+							type="password"
+							placeholder="패스워드"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</S.LoginField>
+					<S.StyledButton variant="outline-primary" onClick={handleLoginClick}>
+						Login
+					</S.StyledButton>
+				</S.Group>
+			) : (
+				<>
+					<S.Paragraph>로그인 아이디 : {user}</S.Paragraph>
+					<S.StyledButton variant="outline-primary" onClick={handleLogout}>
+						Logout
+					</S.StyledButton>
+				</>
+			)}
+		</S.Container>
+	);
 };
 
 export default Login;
