9e52ad754223520689bbe38e448a786398780b86
thkim0515 <thkim0515@naver.com>
2024-07-14 13:31:40 +0900

    feat:테스트컴포넌트 MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,30 +1,10 @@
 import React from "react";
 import * as S from "./MainPage.style";
-import StudentManagePage from "../../components/StudentManagePage/StudentManagePage";
-import CommentComponent from "../../components/Common/Comment/CommentComponent";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-const MainPage = () => {
-  const localAddress = useSelector((state) => state.localAddress.value);
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(`${localAddress}users/id/1`);
-        setData(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+const MainPage = () => {
   return (
     <S.Container>
-      <div>{data ? JSON.stringify(data) : "Loading..."}</div>{" "}
-      <StudentManagePage />
-      <CommentComponent />
+      <div>컴포넌트 테스트는 이 컴포넌트에서 진행</div>
     </S.Container>
   );
 };
