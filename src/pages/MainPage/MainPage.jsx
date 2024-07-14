8af768d7988549240f5ba166384566af24cd6f95
jiandays <yj000747@naver.com>
2024-07-14 21:43:13 +0900

    sja_reviceEducationForm

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import * as S from "./MainPage.style";
 import Settings from "../Settings/Settings";
+//import EducationForm from "../../components/EducationForm/EducationForm";
 
 const MainPage = () => {
-  return (
-    <S.Container>
-      <div>컴포넌트 테스트는 이 컴포넌트에서 진행</div>
-      <Settings />
-    </S.Container>
-  );
+	return (
+		<S.Container>
+			<div>컴포넌트 테스트는 이 컴포넌트에서 진행</div>
+			<Settings />
+			{/*<EducationForm />*/}
+		</S.Container>
+	);
 };
 
 export default MainPage;
