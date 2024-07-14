aa7f1bf764addf9573643514bd6f2aae98c746d2
jiandays <yj000747@naver.com>
2024-07-14 17:52:30 +0900

    sja_reviceSettings

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
-import StudentManage from '../../components/StudentManage/StudentManage';
+import { Tab as BootstrapTab, Tabs as BootstrapTabs } from "react-bootstrap";
+import StudentManage from "../../components/StudentManage/StudentManage";
 import CohortManage from "../../components/CohortManage/CohortManage";
 import BranchManage from "../../components/BranchManage/BranchManage";
+import styled from "styled-components";
+
+const CustomTabs = styled(BootstrapTabs)`
+	background-color: white;
+`;
+
+const CustomTab = styled(BootstrapTab)`
+	color: #555 !important; /* 추가된 스타일이 우선하도록 합니다 */
+
+	&.active {
+		color: blue !important; /* 활성화된 탭의 글씨 색을 지정합니다 */
+	}
+`;
 
 function Settings() {
-    return (
-        <Tabs
-            defaultActiveKey="student-manage"
-            id="uncontrolled-tab-example"
-            className="mb-3"
-        >
-            <Tab eventKey="student-manage" title="학생 등록">
-                <StudentManage />
-            </Tab>
-            <Tab eventKey="cohort-manage" title="기수 등록">
-                <CohortManage />
-            </Tab>
-            <Tab eventKey="branch-manage" title="지점 등록">
-                <BranchManage />
-            </Tab>
-        </Tabs>
-    );
+	return (
+		<CustomTabs
+			defaultActiveKey="student-manage"
+			id="uncontrolled-tab-example"
+			className="mb-3"
+		>
+			<CustomTab eventKey="student-manage" title="학생 등록">
+				<StudentManage />
+			</CustomTab>
+			<CustomTab eventKey="cohort-manage" title="기수 등록">
+				<CohortManage />
+			</CustomTab>
+			<CustomTab eventKey="branch-manage" title="지점 등록">
+				<BranchManage />
+			</CustomTab>
+		</CustomTabs>
+	);
 }
 
 export default Settings;
