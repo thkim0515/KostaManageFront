39b85da2c9f6a1a3752029d2fb518c9e9f3a7e85
thkim0515 <thkim0515@naver.com>
2024-07-15 11:05:46 +0900

    delete

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -6,7 +6,7 @@ const MainPage = () => {
   return (
     <S.Container>
       <div>컴포넌트 테스트는 이 컴포넌트에서 진행</div>
-      {/* <ResearcherMenu /> */}
+      <ResearcherMenu />
       <EducationForm />
     </S.Container>
   );
