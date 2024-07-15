d4e49347912c3cdb1f61316bcb910f0fa7882dae
jiandays <yj000747@naver.com>
2024-07-15 11:56:16 +0900

    sja_reviceEducationForm

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,47 +24,51 @@ const Login = () => {
   const isAdmin = userInfo?.role === "Admin" || userInfo?.role === "Researcher";
 
   return (
-    <S.Container>
-      <S.Group>
-        {!user ? (
-          <>
-            <S.LoginField>
-              <S.Input
-                type="text"
-                placeholder="아이디"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </S.LoginField>
-            <S.LoginField>
-              <S.Input
-                type="password"
-                placeholder="패스워드"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </S.LoginField>
-            <S.StyledButton onClick={handleLoginClick}>Login</S.StyledButton>
-          </>
-        ) : (
-          <>
-            <S.Paragraph>로그인 아이디 : {user}</S.Paragraph>
-            <S.Paragraph>JWT 토큰 : {token}</S.Paragraph>
-            {userInfo && (
-              <div>
-                <S.Paragraph>이메일: {userInfo.email}</S.Paragraph>
-                <S.Paragraph>전화번호: {userInfo.phoneNumber}</S.Paragraph>
-                <S.Paragraph>역할: {userInfo.role}</S.Paragraph>
-                <S.Paragraph>승인 상태: {userInfo.approvalStatus}</S.Paragraph>
-                {isAdmin ? <S.Paragraph>관리자 계정입니다</S.Paragraph> : null}
-              </div>
-            )}
-            <S.StyledButton onClick={handleLogout}>Logout</S.StyledButton>
-          </>
-        )}
-      </S.Group>
-    </S.Container>
-  );
+		<S.Container>
+			<S.Group>
+				<br />
+				<h2>로그인</h2>
+				<br />
+				<br />
+				{!user ? (
+					<>
+						<S.LoginField>
+							<S.Input
+								type="text"
+								placeholder="아이디"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
+						</S.LoginField>
+						<S.LoginField>
+							<S.Input
+								type="password"
+								placeholder="패스워드"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</S.LoginField>
+						<S.StyledButton onClick={handleLoginClick}>Login</S.StyledButton>
+					</>
+				) : (
+					<>
+						<S.Paragraph>로그인 아이디 : {user}</S.Paragraph>
+						<S.Paragraph>JWT 토큰 : {token}</S.Paragraph>
+						{userInfo && (
+							<div>
+								<S.Paragraph>이메일: {userInfo.email}</S.Paragraph>
+								<S.Paragraph>전화번호: {userInfo.phoneNumber}</S.Paragraph>
+								<S.Paragraph>역할: {userInfo.role}</S.Paragraph>
+								<S.Paragraph>승인 상태: {userInfo.approvalStatus}</S.Paragraph>
+								{isAdmin ? <S.Paragraph>관리자 계정입니다</S.Paragraph> : null}
+							</div>
+						)}
+						<S.StyledButton onClick={handleLogout}>Logout</S.StyledButton>
+					</>
+				)}
+			</S.Group>
+		</S.Container>
+	);
 };
 
 export default Login;
